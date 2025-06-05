b6d01d3b87c0324a54b40bb9d428ce0efc5d62bb
Moska002 <ale.moska002@gmail.com>
2025-06-05 23:51:25 +0200

    UPDATE Copy warning

--- loco-run.sh
+++ loco-run.sh
@@ -23,8 +23,12 @@ if [ -z "$controller_name" ]; then
 	exit 1
 fi
 
-cp $LOCOSIM_DIR/robot_descriptions/$package_name/controller/* $LOCOSIM_DIR/robot_control/base_controllers || exit 1
-catkin build $package_name --workspace $HOME/ros_ws
+if [ -d "$LOCOSIM_DIR/robot_descriptions/$package_name/controller" ]; then
+	cp $LOCOSIM_DIR/robot_descriptions/$package_name/controller/* $LOCOSIM_DIR/robot_control/base_controllers || exit 1
+	catkin build $package_name --workspace $HOME/ros_ws
+else
+	echo "WARNING: Package controller directory is not found"
+fi
 
 if [ "$i_option" = true ]; then
     python3 -i $LOCOSIM_DIR/robot_control/base_controllers/$controller_name
